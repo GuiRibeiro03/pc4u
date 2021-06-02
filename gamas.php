aa74277f529b5585940c52a1f96ba4af95c1818f
pipe1910 <pipevital@gmail.com>
2021-06-02 16:44:55 +0100

    Guilherme-Gamas & Pics

--- gamas.php
+++ gamas.php
@@ -2,42 +2,96 @@
 include_once("includes/body.inc.php");
 top(GAMAS);
 ?>
-        
 
-        <section id="clients">
+
+<section id="clients">
 
     <!-- Team Inner -->
-          <div class="inner team">
+    <div class="inner team">
 
-      <!-- Header -->
-              <div class="row col-md-12">
-              <div class="col-md-1">
+        <!-- Header -->
+        <div class="row col-md-12" style="text-align: center">
+            <!-- ------------START: AQUA---------------------- -->
+            <div class="container">
+                <div class="col-md-1">
 
-              </div>
-              <div class="col-md-2 mt-5">
-                  <img class="logo" src="img/aqua.gif" alt="logo" width="250">
-              </div>
-              <div class="col-md-8">
-                  <p>sadsdadsadsdsadasdasdsadasdsa</p>
-              </div>
-              <div class="col-md-1">
+                </div>
 
-              </div>
-                  <div class="col-md-1">
+                <div class="container" style="outline: solid 3px deepskyblue">
+                    <div class="col-md-4 mt-5">
+                        <img class="logo" src="img/aquaLogo.png" alt="logo" width="250">
+                    </div>
+                    <div class="header-text"><h2 style="font-size: 50px; float:left; color:darkturquoise;">Aqua</h2>
+                    </div>
+                    <div class="col-md-6" style="text-align: left; margin-top: 20px">
+                        <p>sadsdadsadsdsadasdasdsadasdsa</p>
+                    </div>
+                </div>
+                <div class="col-md-1">
 
-                  </div>
-                  <div class="col-md-8 ">
-                      <p>sadsdadsadsdsadasdasdsadasdsa</p>
-                  </div>
-                  <div class="col-md-2 mt-5">
-                      <img class="logo" src="img/aqua.gif" alt="logo" width="250">
-                  </div>
-                  <div class="col-md-1">
+                </div>
+            </div>
+            <!-- ------------END: AQUA---------------------- -->
 
-                  </div>
-              </div>
-    </div><!-- End Team Inner -->
-  </section><!-- End Team Section -->
+
+
+
+            <!-- ------------START: MAGNA---------------------- -->
+            <div class="container">
+                <div class="col-md-1">
+
+                </div>
+                <div class="container" style="outline: solid 3px goldenrod">
+                    <div class="header-text"><h2 style="font-size: 50px; float:left; color:goldenrod;">Magna</h2>
+                    </div>
+                    <div class="col-md-6" style="text-align: left; margin-top: 20px">
+                        <p>sadsdadsadsdsadasdasdsadasdsa</p>
+                    </div>
+                    <div class="col-md-4 mt-5">
+                        <img class="logo" src="img/magna.png" alt="logo" width="250">
+                    </div>
+                </div>
+                    <div class="col-md-1">
+
+                    </div>
+                </div>
+
+            <!-- ------------END: MAGNA---------------------- -->
+
+
+
+
+            <!-- ------------START: ROK---------------------- -->
+                <div class="container">
+                    <div class="col-md-1">
+
+                    </div>
+                    <div class="container" style="outline: solid 3px gold; width: 2000px">
+                    <div class="col-md-4 mt-5">
+                        <img class="logo" src="img/rok.png" alt="logo" width="250">
+                    </div>
+                    <div class="header-text"><h2 style="font-size: 40px; float:left; color:gold;">RoK</h2>
+                    </div>
+                    <div class="col-md-6" style="text-align: left; margin-top: 20px; width: 2000px">
+                        <p>sadsdadsadsdsadasdasdsadasdsaadsdadsadsdsadasdasdsadasdsaadsdadsadsdsadasdasdsadasdsaadsdadsadsdsa</p>
+                        <p> dasdasdsadasdsaadsdadsadsdsadasdasdsadasdsaadsdadsadsdsadasdasdsadasdsaadsdadsadsdsadasdasdsadasdsaadsdadsadsdsadasdasdsadasdsaadsdadsadsdsadasdasdsadasdsa</p>
+                    </div>
+                    </div>
+                    <div class="col-md-1">
+
+                    </div>
+                </div>
+            <!-- ------------END: ROK---------------------- -->
+
+
+
+
+
+
+
+            </div>
+        </div><!-- End Team Inner -->
+</section><!-- End Team Section -->
 
 
 <?php
