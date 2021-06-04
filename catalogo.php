7c77749811b32b28c2f07b39ff1acab8c92921d8
Kashigu <orlandojslopes@gmail.com>
2021-06-04 16:22:17 +0100

    Orlando-Inicio catalogo

---
+++ catalogo.php
@@ -0,0 +1,115 @@
+<?php
+include_once("includes/body.inc.php");
+top(CATALOGO);
+?>
+
+
+<section id="clients">
+
+    <!-- Team Inner -->
+    <div class="inner team">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-9">
+                    <div class="col-lg-8">
+                        <p>Mostrar 1-12 de 15 resultados</p>
+                    </div>
+                    <div class="col-lg-4">
+                        <select class="selecao">
+                        <option>Ordenar por popularidade</option>
+                        <option>Ordenar por média de classificação</option>
+                        <option>Ordenar por mais novos</option>
+                        <option>Ordenar por preço: menor para maior</option>
+                        <option>Ordenar por preço: maior para menor</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-lg-10">
+                    <div class="col-lg-4">
+                        <a class="a2"
+                           href="single-listing.php?id=">
+                            <div class="arrange-pic">
+                                <img class="centrinho"
+                                     src="img/1.jpg">
+                            </div>
+                            <div class="textinho">
+                                <h5>PCBOX GAMER SPECIAL EDITION H500P XSERIES</h5>
+
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4">
+                        <a class="arrange-items"
+                           href="single-listing.php?id=>">
+                            <div class="arrange-pic">
+                                <img class="centrinho"
+                                     src="img/2.jpg">
+
+                            </div>
+                            <div class="arrange-text">
+                                <h5>asdadad</h5>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 ">
+                        <a class="arrange-items"
+                           href="single-listing.php?id=>">
+                            <div class="arrange-pic">
+                                <img class="centrinho"
+                                     src="img/3.jpg"
+                                     alt="">
+                            </div>
+                            <div class="arrange-text">
+                                <h5>asdadad</h5>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 ">
+                        <a class="arrange-items"
+                           href="single-listing.php?id=>">
+                            <div class="arrange-pic">
+                                <img class="centrinho"
+                                     src="img/1.jpg"
+                                     alt="">
+                            </div>
+                            <div class="arrange-text">
+                                <h5>asdadad</h5>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 ">
+                        <a class="arrange-items"
+                           href="single-listing.php?id=>">
+                            <div class="arrange-pic">
+                                <img class="centrinho"
+                                     src="img/1.jpg"
+                                     alt="">
+                            </div>
+                            <div class="arrange-text">
+                                <h5>asdadad</h5>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4">
+                        <a class="arrange-items"
+                           href="single-listing.php?id=>">
+                            <div class="arrange-pic">
+                                <img class="centrinho"
+                                     src="img/1.jpg"
+                                     alt="">
+                            </div>
+                            <div class="arrange-text">
+                                <h5>asdadad</h5>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- End Team Inner -->
+</section><!-- End Team Section -->
+
+
+<?php
+bot()
+?>
