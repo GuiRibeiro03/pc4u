0d9e4e7d570b9384d04d40c6155eb95bfa0e3b2d
pipe1910 <pipevital@gmail.com>
2021-06-04 16:24:45 +0100

    Guilherme-Gamas & Pics

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
-
 <?php
-   include_once("includes/body.inc.php");
-   top();
-   ?>
-
+include_once("includes/body.inc.php");
+top();
+?>
 
 
 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="img/slide.png" class="d-block w-100 " alt="..." style="height: 800px">
-        </div>
+
         <div class="carousel-item active">
             <img src="img/slide1.jpg" class="d-block w-100 " alt="..." style="height: 800px">
         </div>
@@ -22,6 +18,9 @@
         </div>
         <div class="carousel-item">
             <img src="img/slide4.jpg" class="d-block w-100  " alt="..." style="height:800px">
+        </div>
+        <div class="carousel-item">
+            <img src="img/slide.png" class="d-block w-100  " alt="..." style="height:800px">
         </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
@@ -35,155 +34,173 @@
 </div>
 
 
-    <div class="container header-start text-center">
+<div class="container header-start text-center">
 
-          <img src="img/logo.png" style="width: 300px; height: 150px; margin-bottom: 2%">
+    <img src="img/logo.png" style="width: 300px; height: 150px; margin-bottom: 2%">
 
-      <h1 class="main-text" style="font-size: 45px; text-shadow: 1px 1px 1px #000000">As melhores máquinas no mercado</h1>
-        <br>
-    <a href="gamas.php"> <button type="button" id="botaoMais" >Saber Mais &nbsp;<i class="fa fa-arrow-right"></i></button></a>
-    </div>
+    <h1 class="main-text" style="font-size: 45px; text-shadow: 1px 1px 1px #000000">As melhores máquinas no mercado</h1>
+    <br>
+    <a href="gamas.php">
+        <button type="button" id="botaoMais">Saber Mais &nbsp;<i class="fa fa-arrow-right"></i></button>
+    </a>
+</div>
 
-    <div class="divider col-sm-12 col-xs-12 col-md-12">
-      <div class="header-text">Produtos Em Destaque</div>
-    </div>
+<div class="divider col-sm-12 col-xs-12 col-md-12">
+    <div class="header-text">Produtos Em Destaque</div>
+</div>
 
-			<section class="blog">
-				<div class="item col-md-4">
-           <div class="blok-read-sm">
-             <a href="single1.html" class="hover-image">
-                <img src="img/sea.jpg" alt="image">
+<section class="blog">
+    <div class="item col-md-4">
+        <div class="blok-read-sm">
+            <a href="single1.html" class="hover-image">
+                <img src="img/CaixaAQUA.jpg" alt="image">
                 <span class="layer-block"></span>
-             </a>
-              <div class="editor-choice">
-                  <a href="#" style="color: white; text-decoration: none;font-size: 14px;font-weight: bold">Destaque</a>
-              </div>
+            </a>
+            <div class="editor-choice">
+                <a href="#" style="color: white; text-decoration: none;font-size: 14px;font-weight: bold">Destaque</a>
+            </div>
             <div class="content-block">
-              <span class="point-caption bg-blue-point"></span>
-              <span class="bottom-line bg-blue-point"></span>
-                <h1>Discovering OM: A visit back to The Vedas</h1>
-                <p style="font-size: 18px">Gregor then turned to look out the window at the dull weather. Drops of rain could pane,which made..</p>
-                <div class="button-main bg-fio-point">read more</div>
-                 <div class="like-wrap">
-                     <a href="#"><i class="fa fa-heart col-red"></i></a><span>224</span>
-                     <a href="#"><i class="fa fa-comment col-green"></i></a><span>89</span>
-                 </div>
-            	</div>
-            </div>
-          </div>
-            <div class="item col-md-4">
-               <div class="blok-read-sm">
-                 <a href="single1.html" class="hover-image">
-                    <img src="img/sea.jpg" alt="image">
-                    <span class="layer-block"></span>
-                 </a>
-                  <div class="editor-choice">
-
-                      <a href="#" style="color: white; text-decoration: none;font-size: 14px;font-weight: bold">Destaque</a>
-                  </div>
-                <div class="content-block">
-                  <span class="point-caption bg-blue-point"></span>
-                  <span class="bottom-line bg-blue-point"></span>
-                    <h1>Discovering OM: A visit back to The Vedas</h1>
-                    <p style="font-size: 18px">Gregor then turned to look out the window at the dull weather. Drops of rain could pane,which made..</p>
-                    <div class="button-main bg-fio-point">read more</div>
-                       <div class="like-wrap">
-                           <a href="#"><i class="fa fa-heart col-red"></i></a><span>224</span>
-                           <a href="#"><i class="fa fa-comment col-green"></i></a><span>89</span>
-                       </div>
-                	   </div>
-                    </div>
-                 </div>
-                <div class="item col-md-4">
-                   <div class="blok-read-sm">
-                       <a href="single1.html" class="hover-image">
-                          <img src="img/sea.jpg" alt="image">
-                          <span class="layer-block"></span>
-                       </a>
-                        <div class="editor-choice">
-                            <a href="#" style="color: white; text-decoration: none;font-size: 14px; font-weight: bold">Destaque</a>
-                        </div>
-                    <div class="content-block">
-                      <span class="point-caption bg-blue-point"></span>
-                      <span class="bottom-line bg-blue-point"></span>
-                        <h1 >Discovering OM: A visit back to The Vedas</h1>
-                        <p style="font-size: 18px">Gregor then turned to look out the window at the dull weather. Drops of rain could pane,which made..</p>
-                        <div class="button-main bg-fio-point">read more</div>
-                         <div class="like-wrap">
-                             <a href="#"><i class="fa fa-heart col-red"></i></a><span>224</span>
-                             <a href="#"><i class="fa fa-comment col-green"></i></a><span>89</span>
-                         </div>
-                    	</div>
-                    </div>
+                <span class="point-caption bg-blue-point"></span>
+                <span class="bottom-line bg-blue-point"></span>
+                <h1>Aqua G </h1>
+                <p style="font-size: 18px">Gregor then turned to look out the window at the dull weather. Drops of rain
+                    could pane,which made..</p>
+                <div style="text-align: center">
+                    <button type="button"
+                            style="width: 200px; height:50px; font-size: 25px; background-color: black; outline: solid 6px orange;">
+                        399,90€
+                    </button>
                 </div>
-            </section>
 
-
-			     <div class="divider col-sm-12 col-xs-12 col-md-12">
-              <div class="header-text">Our <span>Testimonials</span></div>
             </div>
+        </div>
+    </div>
+    <div class="item col-md-4">
+        <div class="blok-read-sm">
+            <a href="single1.html" class="hover-image" ">
+                <img src="img/CaixaFortis.jpg" alt="image">
+                <span class="layer-block"></span>
+            </a>
+            <div class="editor-choice">
+                <a href="#" style="color: white; text-decoration: none;font-size: 14px;font-weight: bold">Destaque</a>
+            </div>
+            <div class="content-block">
+                <span class="point-caption bg-blue-point"></span>
+                <span class="bottom-line bg-blue-point"></span>
+                <h1> Fortis I</h1>
+                <p style="font-size: 18px">Gregor then turned to look out the window at the dull weather. Drops of rain
+                    could pane,which made..</p>
+                <div style="text-align: center">
+                    <button type="button"
+                            style="width: 200px; height:50px; font-size: 25px; background-color: black; outline: solid 6px orange;">
+                        599,90€
+                    </button>
+                </div>
 
-       <section class="testimonial">
-          <div class="col-md-7 testimonial-blog">
-             <div id="wrapper">
+            </div>
+        </div>
+    </div>
+    <div class="item col-md-4">
+        <div class="blok-read-sm">
+            <a href="single1.html" class="hover-image">
+                <img src="img/CaixaROK.jpg" alt="image">
+                <span class="layer-block"></span>
+            </a>
+            <div class="editor-choice">
+                <a href="#" style="color: white; text-decoration: none;font-size: 14px;font-weight: bold">Destaque</a>
+            </div>
+            <div class="content-block">
+                <span class="point-caption bg-blue-point"></span>
+                <span class="bottom-line bg-blue-point"></span>
+                <h1>Rok I</h1>
+                <p style="font-size: 18px">Gregor then turned to look out the window at the dull weather. Drops of rain
+                    could pane,which made..</p>
+                <div style="text-align: center">
+                    <button type="button"
+                            style="width: 200px; height:50px; font-size: 25px; background-color: black; outline: solid 6px orange;">
+                        899,90€
+                    </button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+<section class="testimonial" style="padding-top: 50%">
+    <div class="divider col-sm-12 col-xs-12 col-md-12">
+        <div class="header-text"><span>Contacte-nos</span></div>
+    </div>
+    <div class="col-md-7 testimonial-blog">
+        <div id="wrapper">
             <!-- DEMO -->
-                <div class="testimonials-slider">
-                  <div class="slide">
+            <div class="testimonials-slider">
+                <div class="slide">
                     <div class="testimonials-carousel-thumbnail"><img width="120" alt="" src="images/team1.jpg"></div>
-                      <div class="testimonials-carousel-context">
+                    <div class="testimonials-carousel-context">
                         <div class="testimonials-name">
-                          Rub elvi <span>shuvohabib.com</span>
+                            Rub elvi <span>shuvohabib.com</span>
                         </div>
                         <div class="testimonials-carousel-content">
-                          <p>Sed posuere consectetur est at lobortis. Fusce dapibus, tellus ac cursus commodo.Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis. Fusce dapibus, tellus ac cursus commodo.</p>
+                            <p>Sed posuere consectetur est at lobortis. Fusce dapibus, tellus ac cursus commodo.Cras
+                                mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.
+                                Fusce dapibus, tellus ac cursus commodo.</p>
                         </div>
-                      </div>
                     </div>
+                </div>
 
-                    <div class="slide">
-                      <div class="testimonials-carousel-thumbnail">
+                <div class="slide">
+                    <div class="testimonials-carousel-thumbnail">
                         <img width="120" alt="" src="images/team2.jpg">
-                      </div>
-                      <div class="testimonials-carousel-context">
+                    </div>
+                    <div class="testimonials-carousel-context">
                         <div class="testimonials-name">Jewel Jahan <span>technextit.com</span>
                         </div>
                         <div class="testimonials-carousel-content">
-                        <p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis. Fusce dapibus, tellus ac cursus commodo.</p>
+                            <p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at
+                                lobortis. Fusce dapibus, tellus ac cursus commodo.</p>
                         </div>
-                      </div>
                     </div>
-
-                      <div class="slide">
-                        <div class="testimonials-carousel-thumbnail"><img width="120" alt="" src="images/team3.jpg"></div>
-                          <div class="testimonials-carousel-context">
-                            <div class="testimonials-name">Mark Zuckerberg<span>www.Facebook.com</span></div>
-                              <div class="testimonials-carousel-content">
-                                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal..</p>
-                              </div>
-                            </div>
-                      </div>
-                    </div>
-                 <!-- END DEMO -->
-                  <div class="clearfix"></div>
                 </div>
-              </div>
-              <div class="col-md-4 subscribe-form">
-                <div class="subscribe">
-                  <h4>Subscribe to Newsletter</h4>
-                    <div class="email-field">
-                        <form action="http://demo.nrgthemes.com/">
-                               <div class="email-input">
-                                  <input type="email" placeholder="Enter your email..." required>
-                                  <i class="fa fa-check col-green"></i>
-                               </div>
-                          <button type="submit" class="button-main bg-green">Submit</button>
-                        </form>
-                          <span>We never spam :)</span>
-                    </div>
-                  </div>
-                </div>
-              </section>
 
-   <?php
-   bot()
-   ?>
+                <div class="slide">
+                    <div class="testimonials-carousel-thumbnail"><img width="120" alt="" src="images/team3.jpg"></div>
+                    <div class="testimonials-carousel-context">
+                        <div class="testimonials-name">Mark Zuckerberg<span>www.Facebook.com</span></div>
+                        <div class="testimonials-carousel-content">
+                            <p>It is a long established fact that a reader will be distracted by the readable content of
+                                a page when looking at its layout. The point of using Lorem Ipsum is that it has a
+                                more-or-less normal..</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- END DEMO -->
+            <div class="clearfix"></div>
+        </div>
+    </div>
+    <div class="col-md-4 subscribe-form">
+        <div class="subscribe">
+            <h4>Subscribe to Newsletter</h4>
+            <div class="email-field">
+                <form action="http://demo.nrgthemes.com/">
+                    <div class="email-input">
+                        <input type="email" placeholder="Enter your email..." required>
+                        <i class="fa fa-check col-green"></i>
+                    </div>
+                    <button type="submit" class="button-main bg-green">Submit</button>
+                </form>
+                <span>We never spam :)</span>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+bot()
+?>
